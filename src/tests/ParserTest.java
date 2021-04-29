b4727e2235dd0a38e9159ceed19cc7b09f7eb8a0
Mo'men Hamdy <53626982+Mo-hamdy2000@users.noreply.github.com>
2021-04-29 19:58:11 +0200

    some Junit tests added

--- src/tests/ParserTest.java
+++ src/tests/ParserTest.java
@@ -6,11 +6,9 @@ import java.io.ByteArrayInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import simpleParser.ParseException;
 import simpleParser.SILParser;
 
 class ParserTest {
@@ -23,22 +21,88 @@ class ParserTest {
 	}
 	
 	@Test
-	void testString() throws FileNotFoundException {
+	void testString() {
 		String initialString = "if tt then a := 2 else a := 4";
 	    SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
-	    Assertions.assertDoesNotThrow(() -> {
+	    assertDoesNotThrow(() -> {
 	        parser.Input();
 	      });
 	}
 	
 	@Test
-	void testFile() throws FileNotFoundException {
+	void testFile1() throws FileNotFoundException {
 		FileReader fileReader = new FileReader("src/resources/test1.txt");
     	SILParser.ReInit(fileReader);
-	    Assertions.assertDoesNotThrow(() -> {
+	    assertDoesNotThrow(() -> {
+	        parser.Input();
+	      });
+	      
+	}
+	
+	@Test
+	void testFile2() throws FileNotFoundException {
+		FileReader fileReader = new FileReader("src/resources/test2.txt");
+    	SILParser.ReInit(fileReader);
+	    assertDoesNotThrow(() -> {
 	        parser.Input();
 	      });
 	      
 	}
 
+	@Test
+	void testStringCommandSemicolon() {
+		String initialString = "b := 7; a := ((2+6) - (12-b))";
+	    SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+	    assertDoesNotThrow(() -> {
+	        parser.Input();
+	      });
+	}
+	
+	@Test
+	void testInvalidVariables() {
+	    assertAll("Test invalid variables",
+	    		() -> assertDoesNotThrow(()-> {
+	            	String initialString = "abcede := 1";
+	            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+	            	parser.Input();
+	            }),
+	            () -> assertThrows(simpleParser.TokenMgrError.class, ()-> {
+	            	String initialString = "gadbb := 1";
+	            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+	            	parser.Input();
+	            }),
+	            () -> assertThrows(simpleParser.TokenMgrError.class, ()-> {
+	            	String initialString = "g := 1";
+	            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+	            	parser.Input();
+	            }),
+	            () -> assertThrows(simpleParser.TokenMgrError.class, ()-> {
+	            	String initialString = "abcedes := 1";
+	            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+	            	parser.Input();
+	            })
+	    );
+	}
+	    @Test
+		void testInvalidDigits() {
+		    assertAll("Test invalid digits",
+		    		() -> assertDoesNotThrow(()-> {
+		            	String initialString = "a := 1";
+		            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+		            	parser.Input();
+		            }),
+		            () -> assertThrows(simpleParser.ParseException.class, ()-> {
+		            	String initialString = "a := -1";
+		            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+		            	parser.Input();
+		            }),
+		            () -> assertThrows(simpleParser.ParseException.class, ()-> {
+		            	String initialString = "a := -20";
+		            	SILParser.ReInit(new ByteArrayInputStream(initialString.getBytes()));
+		            	parser.Input();
+		            })
+		    );
+	}
+	
+	
 }
